15b85f883164dc241f5003efec991be4bc92d697
Habibur Rahman <habibur0199mhr@gmail.com>
2023-11-02 18:52:41 +0600

    607. Sales Person

--- SQL.sql
+++ SQL.sql
@@ -9,3 +9,19 @@ WHERE (a1.player_id, DATE_SUB(a1.event_date, INTERVAL 1 DAY))
 IN (
     SELECT a3.player_id, MIN(a3.event_date) from Activity as a3 group by a3.player_id
 );
+
+-- 607. Sales Person
+select 
+  s.name 
+from 
+  SalesPerson as s 
+where 
+  s.sales_id not in (
+    select 
+      o.sales_id 
+    from 
+      Orders as o 
+      left join Company as c on o.com_id = c.com_id 
+    where 
+      c.name = 'RED'
+  );
