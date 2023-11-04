88a177597d223888c61913f3878192070fb65f1f
Habibur Rahman <habibur0199mhr@gmail.com>
2023-11-04 09:27:36 +0600

    S608: TreeNode

--- SQL.sql
+++ SQL.sql
@@ -25,3 +25,15 @@ where
     where 
       c.name = 'RED'
   );
+
+-- 608. Tree Node
+select 
+  id, 
+  case when p_id is null then 'Root' when id in (
+    select 
+      p_id 
+    from 
+      Tree
+  ) then 'Inner' else 'Leaf' end as type 
+from 
+  Tree;
