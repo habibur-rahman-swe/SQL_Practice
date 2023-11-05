312bff76ce9dd0450545cd4d79e13c74035967f2
Habibur Rahman <habibur0199mhr@gmail.com>
2023-11-05 21:00:13 +0600

    619. Biggest Single Number

--- SQL.sql
+++ SQL.sql
@@ -55,3 +55,19 @@ select
 from 
   Triangle;
 
+-- 619. Biggest Single Number
+select 
+  (
+    select 
+      num 
+    from 
+      MyNumbers 
+    group by 
+      num 
+    having 
+      count(num) = 1 
+    order by 
+      num desc 
+    limit 
+      1
+  ) as num;
