e8a5ca2f6b0957f9d22fd0a5428d19422ce69213
Habibur Rahman <habibur0199mhr@gmail.com>
2023-11-06 20:22:42 +0600

    626. Exchange Seats

--- SQL.sql
+++ SQL.sql
@@ -71,3 +71,18 @@ select
     limit 
       1
   ) as num;
+
+-- 626. Exchange Seats
+select 
+  case when id = (
+    select 
+      max(id) 
+    from 
+      seat
+  ) 
+  and id mod 2 = 1 then id when id mod 2 = 1 then id + 1 else id - 1 end as id, 
+  student 
+from 
+  seat 
+order by 
+  id;
